6e09aff4a059e4c79d7490f3bf19ab72cc9e7f9e
Christopher <holomekc.github@gmail.com>
2020-02-28 21:56:37 +0100

    (holomekc) cleanup

--- src/builder/https-config.ts
+++ src/builder/https-config.ts
@@ -2,35 +2,4 @@ export interface HttpsConfig {
     port: number;
     key: string;
     cert: string;
-
-    // constructor(port: number, key: string, cert: string) {
-    //     this._port = port;
-    //     this._key = key;
-    //     this._cert = cert;
-    // }
-    //
-    //
-    // set port(value: number) {
-    //     this._port = value;
-    // }
-    //
-    // set key(value: string) {
-    //     this._key = value;
-    // }
-    //
-    // set cert(value: string) {
-    //     this._cert = value;
-    // }
-    //
-    // get port(): number {
-    //     return this._port;
-    // }
-    //
-    // get key(): string {
-    //     return this._key;
-    // }
-    //
-    // get cert(): string {
-    //     return this._cert;
-    // }
 }
