8662c104c9ce0a20c468ce6ea755b1849b1a4b8c
holomekc <30546982+holomekc@users.noreply.github.com>
2023-12-22 18:02:10 +0100

    (holomekc) add some docs

--- src/server/lib/hue-s.ts
+++ src/server/lib/hue-s.ts
@@ -7,21 +7,55 @@ export abstract class HueS {
     protected constructor(protected builder: HueBuilder) {
     }
 
+    /**
+     * Set headers used for every response
+     * @param param headers to set
+     */
     abstract setDefaultResponseHeaders(param: { [key: string]: string }): void;
 
+    /**
+     * Register hook for incoming requests
+     * @param callback callback function to call
+     */
     abstract registerOnRequest(callback: (req: HueSRequest) => void): void;
 
-    abstract registerOnRequest(callback: (req: HueSRequest) => void): void;
-
+    /**
+     * Register hook for outgoing responses
+     * @param callback callback function to call
+     */
     abstract registerOnResponse(callback: (payload: any, headers: any) => void): void;
 
+    /**
+     * Register HTTP GET call
+     * @param path url path to register
+     * @param callback callback function to call
+     */
     abstract get(path: string, callback: (req: HueSRequest, res: HueSResponse) => void): void;
 
+    /**
+     * Register HTTP POST call
+     * @param path url path to register
+     * @param callback callback function to call
+     */
     abstract post(path: string, callback: (req: HueSRequest, res: HueSResponse) => void): void;
 
+    /**
+     * Register HTTP PUT call
+     * @param path url path to register
+     * @param callback callback function to call
+     */
     abstract put(path: string, callback: (req: HueSRequest, res: HueSResponse) => void): void;
 
+    /**
+     * Register HTTP DELETE call
+     * @param path url path to register
+     * @param callback callback function to call
+     */
     abstract delete(path: string, callback: (req: HueSRequest, res: HueSResponse) => void): void;
 
+    /**
+     * Start server
+     * @param onReady callback to call when server is ready
+     */
     abstract startServer(onReady: () => void): void;
 }
